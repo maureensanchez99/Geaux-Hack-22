832e3a90b96c7dd6718aedfb434dc6ec5c083604
Maureen Sanchez <39846411+maureensanchez99@users.noreply.github.com>
2022-10-01 17:00:39 -0500

    trying to

--- sase-project/src/App.js
+++ sase-project/src/App.js
@@ -3,26 +3,12 @@ import {Routes, Route, useNavigate} from 'react-router-dom';
 import LiveData from './LiveData';
 
 function App(){
-  const navigate = useNavigate();
-
-  const navigateToMap = () => {
-    navigate('/LiveData');
-  }
-
-  const navigateHome = () => {
-    navigate('/');
-  }
-
   return(
     <div>
       <div className="container">
-        <h1 className="header">Tiger</h1>
-        {/* <button onClick={navigateToMap}>Live Map</button> */}
+        <h1 className="header">Tiger Trax</h1>
+        <button onClick={navigateToMap}>Live Map</button>
       </div>
-
-      {/* <Routes> */}
-        {/* <Route path="/" element={<LiveData/>}></Route> */}
-      {/* </Routes> */}
     </div>
   );
 }
