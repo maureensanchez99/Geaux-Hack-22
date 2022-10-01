bb19f054b146b783fd11412605bf6e9363893ebf
Maureen Sanchez <39846411+maureensanchez99@users.noreply.github.com>
2022-10-01 16:41:33 -0500

    created button

--- sase-project/src/App.js
+++ sase-project/src/App.js
@@ -1,9 +1,27 @@
 import './App.css';
+import {Routes, Route, useNavigate} from 'react-router-dom';
 
 function App(){
+  const navigate = useNavigate();
+
+  const navigateToMap = () => {
+    navigate('/LiveData');
+  }
+
+  const navigateHome = () => {
+    navigate('/');
+  }
+
   return(
-    <div className="container">
-      <h1 className="header">Tiger</h1>
+    <div>
+      <div className="container">
+        <h1 className="header">Tiger</h1>
+        <button onClick={navigateToMap}>Live Map</button>
+      </div>
+
+      <Routes>
+        <Route path="/LiveData" element={<LiveData/>}></Route>
+      </Routes>
     </div>
   );
 }
