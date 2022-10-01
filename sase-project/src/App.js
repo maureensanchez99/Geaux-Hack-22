769acfb6c52bd8859ef09b9ed4d88c133659a2a6
Maureen Sanchez <39846411+maureensanchez99@users.noreply.github.com>
2022-10-01 16:12:57 -0500

    adjusting home page

--- sase-project/src/App.js
+++ sase-project/src/App.js
@@ -1,17 +1,12 @@
 import './App.css';
 
-<html>
-  <head>
-    <title> LSU </title>
-    <title> Tiger Trax </title>
-  </head>
-  <body> 
-    <img id = " " src = " " style = "width: 100px" ></img> // blank image file check accept type
-    <button onclick = " "> Park Here </button>
-    <button onclick = "LiveData.js"> View Live Map </button>
-  </body>
-
-</html> 
+function App(){
+  return(
+    <div className="container">
+      <h1 className="header">Tiger Trax</h1>
+    </div>
+  );
+}
 
 export default App;
 
