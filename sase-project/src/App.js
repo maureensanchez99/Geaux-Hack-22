7a92ef561098e871a39cacf8f5506fc029b2413f
Maureen Sanchez <39846411+maureensanchez99@users.noreply.github.com>
2022-10-01 00:50:49 -0500

    trying to utilizae TomTom api

--- sase-project/src/App.js
+++ sase-project/src/App.js
@@ -1,10 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { useEffect, useState, useRef } from "react";
+import axios from "axios";
+import "./App.css";
+// TomTom SDK
+import * as tt from "@tomtom-international/web-sdk-maps";
+// styles
+import "@tomtom-international/web-sdk-maps/dist/maps.css";
+
 function App() {
+  const [map, setMap] = useState();
+  const mapContainer = useRef();
+  const AMSTERDAM = { lon: 4.896029, lat: 52.371807 };
+
+  useEffect(() => {
+    let map = tt.map({
+      key: <qgsuBcvctE2xWrChbABEv2mcoxa21qoV>,
+      container: mapContainer.current.id,
+      center: AMSTERDAM,
+      zoom: 10,
+      language: "en-GB",
+    });
+    map.addControl(new tt.FullscreenControl());
+    map.addControl(new tt.NavigationControl());
+
+    setMap(map);
+    return () => {
+      map.remove();
+    };
+    //eslint-disable-next-line
+  }, []);
+
   return (
     <div className="container">
-      <h3 className="header">Tiger Trax</h3>
+      <nav className="nav">
+        <h1>Tiger Trax</h1>
+      </nav>
+      <div ref={mapContainer} className="map" id="map" />
     </div>
   );
 }
