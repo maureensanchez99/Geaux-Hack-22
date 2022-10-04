40d96bdb72a20ca67ff2ecf7041960edca7f3449
Maureen Sanchez <39846411+maureensanchez99@users.noreply.github.com>
2022-10-04 11:17:09 -0500

    checking up on deployment

--- sase-project/src/App.js
+++ sase-project/src/App.js
@@ -1,13 +1,11 @@
 import './App.css';
-import {Routes, Route, useNavigate} from 'react-router-dom';
-import LiveData from './LiveData';
 
 function App(){
   return(
     <div>
       <div className="container">
         <h1 className="header">Tiger Trax</h1>
-        <button onClick={navigateToMap}>Live Map</button>
+        <a href='Geaux-Hack-22/LiveData' className='btn'>Live Map</a>
       </div>
     </div>
   );
